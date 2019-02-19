988d0376bfc2177c448331a02070c7e51a56d610
PPODO <sherlockwhooo@gmail.com>
2019-02-20 01:54:09 +0900

    Revert "Revert "no message""
    This reverts commit d745f1797ca5b57e3426075fccd5a471b036a055.

--- GameFrameWork/InputClass.h
+++ GameFrameWork/InputClass.h
@@ -3,19 +3,30 @@
 #include <functional>
 #include <map>
 
-typedef std::function<void(float)> Function;
+typedef std::function<void(float)> AxisFunction;
+typedef std::function<void()> ActionFunction;
+
+enum EActionInputState { IE_Pressed, IE_Released };
 
 #pragma pack(push, 1)
 typedef struct {
 	bool m_bIsPressed;
-	Function m_Delegate;
-}DelegateInfomations;
+	AxisFunction m_Delegate;
+}DelegateAxisInfomations;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+	EActionInputState m_InputState;
+	ActionFunction m_Delegate;
+}DelegateActionInfomations;
 #pragma pack(pop)
 
 class InputClass {
 private:
 	bool m_Keys[256];
-	std::map<unsigned short, DelegateInfomations> m_BindKeys;
+	std::map<unsigned short, DelegateAxisInfomations> m_BindAxisKeys;
+	std::map<unsigned short, DelegateActionInfomations> m_BindActionKeys;
 
 public:
 	InputClass();
@@ -26,6 +37,7 @@ public:
 	void KeyIsUp(unsigned short Key);
 
 public:
-	void BindDelegate(unsigned short Key, Function Delegate);
+	void BindAxisDelegate(unsigned short Key, AxisFunction Delegate);
+	void BindActionDelegate(unsigned short Key, EActionInputState InputState, ActionFunction Delegate);
 
 };
--- GameFrameWork/PlayerClass.cpp
+++ GameFrameWork/PlayerClass.cpp
@@ -9,7 +9,8 @@ PlayerClass::~PlayerClass() {
 bool PlayerClass::Init(LPDIRECT3DDEVICE9 Device, LPCWSTR ImageSrc, bool bUseCustomRect, RECT CustomRect) {
 	Actor::Init(Device, L"Player.png");
 
-	SystemClass::GetInst()->GetInputClass()->BindDelegate(VK_RIGHT, std::bind(&PlayerClass::MoveRight, this, 1.f));
+	SystemClass::GetInst()->GetInputClass()->BindAxisDelegate(VK_RIGHT, std::bind(&PlayerClass::MoveRight, this, 1.f));
+	SystemClass::GetInst()->GetInputClass()->BindActionDelegate(VK_SPACE, IE_Pressed, std::bind(&PlayerClass::PopupMessageBox, this));
 	return true;
 }
 
@@ -28,4 +29,8 @@ void PlayerClass::Destroy() {
 
 void PlayerClass::MoveRight(float Value) {
 	m_Image->m_Position.x += Value * 2;
+}
+
+void PlayerClass::PopupMessageBox() {
+	MessageBox(SystemClass::GetInst()->GetWindowHandle(), L"Pressed Space Bar!!", L"HI!", MB_OK);
 }
